58cce9d877f112f807113712a4f0990d7b0e3f36
Paul S <paull@Pauls-MBP.fios-router.home>
2018-01-10 21:59:57 -0500

    quick start

---
+++ scripts/fish.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+brew install fish && \
+curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher 
+
---
+++ scripts/go.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+mkdir -p $HOME/go/src/github.com/paulsevere $HOME/go/bin $HOME/go/pkg
